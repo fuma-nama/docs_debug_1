4092fb9a849f0a2fdb0060d8a8c47632deecf784
wolone <71954466@qq.com>
2024-09-20 21:10:39 +0800

    📝 更新文档

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -2,10 +2,31 @@ import { DocsLayout } from 'fumadocs-ui/layout';
 import type { ReactNode } from 'react';
 import { baseOptions } from '../layout.config';
 import { source } from '@/app/source';
+import { RootToggle } from 'fumadocs-ui/components/layout/root-toggle';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <DocsLayout tree={source.pageTree} {...baseOptions}>
+    <DocsLayout 
+    // root-toggle 根文档切换
+    sidebar={{
+      banner: (
+        <RootToggle
+          options={[
+            {
+              title: 'Framework',
+              description: '开源低代码框架',
+              url: '/docs',
+            },
+            {
+              title: 'ERPNext',
+              description: '全球最好的免费开源ERP',
+              url: '/docs',
+            },
+          ]}
+        />
+      ),
+    }}
+    tree={source.pageTree} {...baseOptions}>
       {children}
     </DocsLayout>
   );
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -9,13 +9,20 @@ import { type HomeLayoutProps } from 'fumadocs-ui/home-layout';
  */
 export const baseOptions: HomeLayoutProps = {
   nav: {
-    title: 'My App',
+    title: 'Frappe',
   },
   links: [
     {
-      text: 'Documentation',
-      url: '/docs',
-      active: 'nested-url',
+      text: 'Home',
+      url: '/',
+    },
+    {
+      text: 'Framework',
+      url: '/framework',
+    },
+    {
+      text: 'ERPNext',
+      url: '/erpnext',
     },
   ],
 };
